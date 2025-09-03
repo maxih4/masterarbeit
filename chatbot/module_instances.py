f30312d686bb6bd68dc8bfbeae7d904cc038df22
Max Handke <handkemax@gmail.com>
2025-09-03 16:28:44 +0200

    updated env variables and changed anonymzation replace pattern

--- chatbot/module_instances.py
+++ chatbot/module_instances.py
@@ -2,7 +2,12 @@ import os
 
 from dotenv import load_dotenv
 from langchain_experimental.data_anonymizer import PresidioAnonymizer
-from langchain_openai import ChatOpenAI
+from langchain_openai import (
+    ChatOpenAI,
+    OpenAIEmbeddings,
+    AzureOpenAIEmbeddings,
+    AzureChatOpenAI,
+)
 from presidio_anonymizer import OperatorConfig
 
 from modules.anonymizer_manager import AnonymizerManager
@@ -15,24 +20,36 @@ from psycopg_pool import AsyncConnectionPool
 load_dotenv()
 
 model_manager = ModelManager(
-    llm_model_classify=ChatOpenAI(
+    llm_model_classify=AzureChatOpenAI(
         model=os.environ.get("OPENAI_MODEL_NAME_CLASSIFY"),
         temperature=0,
         api_key=os.environ.get("OPENAI_API_KEY_CLASSIFY"),
-        base_url=os.environ.get("OPENAI_API_URL_CLASSIFY"),
+        azure_endpoint=os.environ.get("OPENAI_API_ENDPOINT_CLASSIFY"),
+        azure_deployment=os.environ.get("OPENAI_API_DEPLOYMENT_CLASSIFY"),
+        api_version=os.environ.get("OPENAI_API_VERSION_CLASSIFY"),
+        max_tokens=None,
     ),
     # embedding_model=HuggingFaceEmbeddings(
     #     model_name="Qwen/Qwen3-Embedding-8B",
     #     model_kwargs={"trust_remote_code": True},
     # ),
-    embedding_model=HuggingFaceEmbeddings(
-        model_name="sentence-transformers/all-MiniLM-L6-v2",
+    # embedding_model=HuggingFaceEmbeddings(
+    #    model_name="sentence-transformers/all-MiniLM-L6-v2",
+    # ),
+    embedding_model=AzureOpenAIEmbeddings(
+        model=os.environ.get("EMBEDDING_MODEL_NAME"),
+        azure_endpoint=os.environ.get("EMBEDDING_MODEL_API_ENDPOINT"),
+        api_key=os.environ.get("EMBEDDING_MODEL_API_KEY"),
+        azure_deployment=os.environ.get("EMBEDDING_MODEL_DEPLOYMENT"),
     ),
-    llm_model=ChatOpenAI(
+    llm_model=AzureChatOpenAI(
         model=os.environ.get("OPENAI_MODEL_NAME"),
         temperature=0,
         api_key=os.environ.get("OPENAI_API_KEY"),
-        base_url=os.environ.get("OPENAI_API_URL"),
+        azure_endpoint=os.environ.get("OPENAI_API_ENDPOINT"),
+        azure_deployment=os.environ.get("OPENAI_API_DEPLOYMENT"),
+        api_version=os.environ.get("OPENAI_API_VERSION"),
+        max_tokens=None,
     ),
 )
 
@@ -47,7 +64,7 @@ def create_db_manager(drop_old: bool = False) -> DatabaseManager:
             auto_id=True,
             drop_old=drop_old,
             enable_dynamic_field=True,
-            collection_name="simple_model",
+            collection_name=os.environ.get("VECTOR_COLLECTION_NAME"),
         ),
         conn_pool=AsyncConnectionPool(
             conninfo="postgresql://postgres:example@localhost:5432/postgres?sslmode=disable",
@@ -67,10 +84,16 @@ anonymizer_manager = AnonymizerManager(
             ],
         },
         operators={
-            "PHONE_NUMBER": OperatorConfig("replace", {"new_value": "<ANONYMIZED>"}),
-            "LOCATION": OperatorConfig("replace", {"new_value": "<ANONYMIZED>"}),
-            "EMAIL_ADDRESS": OperatorConfig("replace", {"new_value": "<ANONYMIZED>"}),
-            "PERSON": OperatorConfig("replace", {"new_value": "<ANONYMIZED>"}),
+            "PHONE_NUMBER": OperatorConfig(
+                "replace", {"new_value": "<ANONYMIZED_PHONE>"}
+            ),
+            "LOCATION": OperatorConfig(
+                "replace", {"new_value": "<ANONYMIZED_LOCATION>"}
+            ),
+            "EMAIL_ADDRESS": OperatorConfig(
+                "replace", {"new_value": "<ANONYMIZED_MAIL>"}
+            ),
+            "PERSON": OperatorConfig("replace", {"new_value": "<ANONYMIZED_PERSON>"}),
         },
     )
 )
